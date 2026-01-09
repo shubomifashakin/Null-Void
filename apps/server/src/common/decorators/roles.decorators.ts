a7ba4b0d51c2c3ebf3c993d077875d1017990f62
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-01-09 22:59:51 +0100

    refactor

--- apps/server/src/common/decorators/roles.decorators.ts
+++ apps/server/src/common/decorators/roles.decorators.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { Roles as PrismaRoles } from 'generated/prisma/enums';
+import { Roles as PrismaRoles } from '../../../generated/prisma/enums';
 
 export const ROLE_KEY = 'roles';
 export const Roles = (...roles: PrismaRoles[]) => SetMetadata(ROLE_KEY, roles);
