a09ed6b25dee92c5299f26cdc4d920080a8abbda
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-01-09 21:55:40 +0100

    updated prisma client filter

--- apps/server/src/common/filters/prisma-known-error.filter.ts
+++ apps/server/src/common/filters/prisma-known-error.filter.ts
@@ -10,7 +10,12 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 export class PrismaKnownErrorFilter implements ExceptionFilter {
   catch(exception: PrismaClientKnownRequestError): void {
     if (exception.code === 'P2025') {
-      throw new NotFoundException('Record does not exist');
+      const modelName =
+        typeof exception?.meta?.modelName === 'string'
+          ? exception.meta.modelName
+          : 'Record';
+
+      throw new NotFoundException(`${modelName} does not exist`);
     }
 
     throw new InternalServerErrorException('Internal server error');
