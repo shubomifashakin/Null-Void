62a25fd0d8776f836ee202e1480bebdba2346ebf
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-01-23 11:53:39 +0100

    refactor

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ images: {
+    qualities: [100, 75],
+    remotePatterns: [{ hostname: "avatars.githubusercontent.com" }],
+  },
 };
 
 export default nextConfig;
