b92c1ba7a42a943bdcff1c8719692a593bb79b19
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-01-23 13:24:57 +0100

    updated next config

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */ images: {
+  images: {
     qualities: [100, 75],
-    remotePatterns: [{ hostname: "avatars.githubusercontent.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
